98db548741b9e507b415efb18e94162ca2b0c1ec
Muhammad Kaif <kaif88679@gmail.com>
2024-09-27 06:22:19 +0100

    first commit

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-function call(){
-var totalMarks = document.getElementById("input").value.trim();
-var numericMarks = parseFloat(totalMarks);
-
-if (isNaN(numericMarks)) {
-    document.getElementById("errorgen").innerHTML = "Invalid value:Total Marks is not a number";
-    return;
-}
-
-var ObtainedMarks = document.getElementById("input1").value.trim();
-var numericMarks1 = parseFloat(ObtainedMarks);
-
-if (isNaN(numericMarks1)) {
-    document.getElementById("errorgen").innerHTML = "Invalid value:Obtained Marks is  not a number";
-    return;
-}
-else if (numericMarks1 > numericMarks) {
-    document.getElementById("errorgen").innerHTML = `Invalid value ! Obtained Marks is greater than total marks`;
-    return;
-} 
-else if (numericMarks < numericMarks1) {
-    document.getElementById("errorgen").innerHTML = `Invalid value ! Obtained Marks is less than total marks`;
-    return;
-} 
-else {
-    document.getElementById("errorgen").innerHTML = ""; 
-}
-
-    var percentage1 = numericMarks1*100/numericMarks;
-    percentage2 =(Math.floor(percentage1));
-    document.getElementById("Percentage").innerHTML=`Your obtained marks is ${numericMarks1} and your percentage is  ${percentage2} :`;
-
-    if (percentage2 > 90){
-        document.getElementById("grade").innerHTML=" Congralution! <br> your marks is highest";
-    }
-    else if(percentage2 > 80 && percentage2 <= 90){
-        document.getElementById("grade").innerHTML=" Congralution! <br> your grade is A-one";
-    }
-    else if(percentage2 > 70 && percentage2 <= 80){
-        document.getElementById("grade").innerHTML=" Congralution! <br> your grade is A";
-    }
-    else if(percentage2 > 60 && percentage2 <= 70){
-        document.getElementById("grade").innerHTML=" Congralution! <br> your grade is B";
-    }
-    else if(percentage2 >= 50 && percentage2 <= 60){
-        document.getElementById("grade").innerHTML=" Congralution! <br> your grade is C";
+function Cal() {
+    var totalMarks =document.getElementById("input").value;
+    console.log(totalMarks);
+    var userMarks =document.getElementById("input1").value;
+    console.log(userMarks);
+    var showPercentage =document.getElementById("showPercentage");
+    var showGrade =document.getElementById("Showgrade");
+    var calculatePercentage = parseFloat((userMarks/totalMarks)*100);
+    console.log(calculatePercentage);
+    if(calculatePercentage <= 100 && calculatePercentage >= 0){
+        showPercentage.innerHTML=`Your Percentage is ${calculatePercentage}%`;
     }
     else{
-        document.getElementById("grade").innerHTML=" Try Again! <br> oooooops fail !";
+        showPercentage.innerHTML=" ";
+    }
+
+    if (calculatePercentage < 49 && calculatePercentage > 0 ) {
+        showGrade.innerHTML=`Try Again : You are fail`;
+    }
+    else if(calculatePercentage >= 50 && calculatePercentage < 60){
+        showGrade.innerHTML=`Congralution : Your grade is C`;
+    }
+    else if(calculatePercentage >= 60 && calculatePercentage < 70){
+        showGrade.innerHTML=`Congralution : Your grade is B`;
+    }
+    else if(calculatePercentage >=70 && calculatePercentage < 80){
+        showGrade.innerHTML=`Congralution : Your grade is A`;
+    }
+    else if(calculatePercentage >=80 && calculatePercentage < 90){
+        showGrade.innerHTML=`Congralution : Your grade is A<sup>+<sup>`;
+    }
+    else if(calculatePercentage >=90 && calculatePercentage < 100){
+        showGrade.innerHTML=`Congralution : you are the yop of list`;
+    }
+    else if (!totalMarks && !userMarks) {
+        showGrade.innerHTML=`Please enter your value`;
+    } 
+    else{
+        showGrade.innerHTML=`Invalid value`;
     }
 }
